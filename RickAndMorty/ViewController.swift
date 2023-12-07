4832a4c7e8768d2f84a913b9f91eb4e4631aa210
Nebahat Oztas <nebussimdi@gmail.com>
2023-12-07 15:33:00 +0300

    initial commit

---
+++ RickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorty
+//
+//  Created by E L F L O O R A   on 7.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
